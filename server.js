11de64a7750f944e0c7e71ddf9ce1987d967de47
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-12-24 12:13:52 +0630

    completed

--- server.js
+++ server.js
@@ -7,23 +7,27 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get("/index", function(req , res){
-//     res.sendFile(__dirname + "/index.html");
-// });
+app.get("/", function(req , res){
+    res.sendFile(__dirname + "/index.html");
+});
 
-app.get("/",function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=702ac1f6e69221b14ffd1c762ea94677";
-    https.get(url,function(response){
-        console.log(response);
+app.post("/",function(req,res){
+   
+   const appid="702ac1f6e69221b14ffd1c762ea94677";
+   const query =req.body.cityinput;
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid="+appid +""
+https.get(url,function(response){
+    console.log(response);
 
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].main
-            console.log("present the weather is " + weatherDescription);
-            console.log("The present temprature is :"+temp);
-        })
-    })
+    response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDescription = weatherData.weather[0].main;
+        res.write("<h1>The present temprature is :"+ temp  +".</h1>");
+        res.write("present the weather is " + weatherDescription);
+        res.send()
+    });
+});
 })
 
 app.listen(3000, function(){
